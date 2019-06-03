a329ca99d002b7d9325b77e8c20a9e9ede1fe364
Corey Moore <monorthwest@gmail.com>
2019-06-03 12:43:39 -0700

    added parameters to function and called function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+'use strict';
+
 function createGreeting() {
-    console.log('Hi, my name is Adrienne and I\'m 26 years old.');
+  return 'Hi, my name is Adrienne and I\'m 26 years old.';
 }
 
-createGreeting();
+let output = createGreeting();
+console.log(output);
