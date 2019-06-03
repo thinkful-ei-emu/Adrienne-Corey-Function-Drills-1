c7cc7067084fa5535dead1db5700d9c73e838ff5
Corey Moore <monorthwest@gmail.com>
2019-06-03 13:01:32 -0700

    Added yearOfBirth variable

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I'm ${age} years old.`;
+  const yearOfBirth = 2019 - age;
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ` + [yearOfBirth];
 }
 
-let output = createGreeting();
+let output = createGreeting('Corey', 30);
 console.log(output);
