f0a5668990bef40c44ff3d40c9c9aad0274a7e0d
Corey Moore <monorthwest@gmail.com>
2019-06-03 14:07:10 -0700

    adding try and catch

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 'use strict';
 
 function getYearOfBirth(age) {
+  if(age < 0) 
+    throw new Error('Age can not be negative');
   return 2019 - age;
 }
 
+
+
 function createGreeting(name, age) {
-  const yearOfBirth = getYearOfBirth(age);
-  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ` + [yearOfBirth];
+  if(age === isNaN || name === ''){
+    throw new Error('Arguments not valid');
+  }
+  try { 
+    const yearOfBirth = getYearOfBirth(age);
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ` + [yearOfBirth];
+  } 
+  catch(e){
+    console.log(e);
+  }
 }
 
-let output = createGreeting('Corey', 30);
+let output = createGreeting('Corey');
 console.log(output);
